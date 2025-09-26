e8f39d3fbe79df64f2347d74d1be6859201b3761
Abdoul03 <samabdoul03@gmail.com>
2025-09-26 11:24:47 +0000

    finalisation des services enfant et parrain

--- src/main/java/com/djiguiya/djiguiya/entity/Enfant.java
+++ src/main/java/com/djiguiya/djiguiya/entity/Enfant.java
@@ -1,5 +1,6 @@
 package com.djiguiya.djiguiya.entity;
 
+import com.djiguiya.djiguiya.dto.responseDto.ChildResponseDTO;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,8 +15,14 @@ import lombok.Setter;
 public class Enfant extends Utilisateurs{
     @Column(nullable = false)
     private String classe;
+    @Column(nullable = false)
+    private int age;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "association_id", nullable = false)
     private Association association;
+
+    /*public ChildResponseDTO  toResponse(){
+        return new ChildResponseDTO(getId(),getNom(),getPrenom(),getUsername(),getClasse(),getAge(),getTelephone(),getEmail(),getGenre(),getRole(), getAssociation().getId());
+    }*/
 }
--- src/main/java/com/djiguiya/djiguiya/service/UtilisateurService.java
+++ src/main/java/com/djiguiya/djiguiya/service/UtilisateurService.java
@@ -1,5 +1,9 @@
 package com.djiguiya.djiguiya.service;
 
+import com.djiguiya.djiguiya.dto.mapper.UtilisateurMapper;
+import com.djiguiya.djiguiya.dto.requestDto.UtilisateurRegisteurDto;
+import com.djiguiya.djiguiya.dto.responseDto.UtilisateurResponseDTO;
+import com.djiguiya.djiguiya.entity.Utilisateurs;
 import com.djiguiya.djiguiya.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,5 +17,14 @@ public class UtilisateurService {
 
     private UserRepository userRepository;
 
+    //Get AllUsers
+    public List<UtilisateurResponseDTO> getAllUsers(){
+        List<Utilisateurs> user = userRepository.findAll();
+        return user.stream().map(UtilisateurMapper::toDTO).toList();
+    }
+    //Get a user
+    public UtilisateurResponseDTO getUser(int userId){
+
+    }
 
 }
